cc052636eed2df862311c37381c92e85e650353e
douglas18208 <carrillo.douglas.18208@gmail.com>
2025-07-27 21:16:16 -0700

    added character counting and sorting

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from stats import *
 
 def main():
-    book_words = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_words = get_book_text(book_path)
     word_count = word_counter(book_words)
-    print(f"{word_count} words found in the document")
-    print(character_counter(book_words))
+    sorted_characters = character_sorter(character_counter(book_words))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("----------- Character Count -----------")
+    for item in sorted_characters:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
 
 
 
--- stats.py
+++ stats.py
@@ -6,8 +6,21 @@ def character_counter(book_words):
     char_count = {}
     lower_words = book_words.lower()
     for char in lower_words:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
     return char_count
+
+def character_sorter(char_count):
+    sorted_char = []
+    for char in char_count:
+        count = char_count[char]
+        single_char = {"char": char, "num": count}  
+        sorted_char.append(single_char)
+    
+    def sort_by_num(single_char):
+        return single_char["num"]
+    sorted_char.sort(key=sort_by_num, reverse=True)
+    return sorted_char
