2d5e8bf9760cdbc0ef370697e8e2fe04cb750736
douglas18208 <carrillo.douglas.18208@gmail.com>
2025-07-27 21:32:41 -0700

    added sys arguments for bookpath

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from stats import *
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+
+
+
     book_words = get_book_text(book_path)
     word_count = word_counter(book_words)
     sorted_characters = character_sorter(character_counter(book_words))
